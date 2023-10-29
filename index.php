55162ea726554637fd809d820b1d37a9784d1116
FabTec <137073373+fabtec-ifc@users.noreply.github.com>
2023-10-29 03:06:42 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="teste">Teste</h1>
+    <button onclick="teste();">OK</button>
+</body>
+</html>
